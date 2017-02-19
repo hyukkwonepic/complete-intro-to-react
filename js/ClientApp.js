3468d6a3d660341e39db10a888ea668622ba1aa1
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-19 13:50:42 +0900

    init MyTitle class component:
    With one component made as class, multiple component instances can be put under the other component.

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,9 +1,24 @@
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
+var MyTitle = React.createClass({
+  //enhanced object literal syntax
+  render () {
+    return (
+      div(null,
+        h1(null, 'Check out this other thing')
+      )
+    )
+  }
+})
+
 var MyFirstComponent = (
-  div({style:{color:'red'}},
-    h1(null, 'This is my first component')
+  div(null,
+    React.createElement(MyTitle, null),
+    React.createElement(MyTitle, null),
+    React.createElement(MyTitle, null),
+    React.createElement(MyTitle, null),
+    React.createElement(MyTitle, null)
   )
 )
 
