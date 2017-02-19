c9e94abebe0ef966cd402ca0a53d90ddfd5fa578
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-20 01:33:04 +0900

    update component from js to jsx:
    Instead of using js, I changed the component file to jsx which can now
    be transpiled as js through babel.
    
    In jsx you can actually include html code inside the compomenent. So you
    don't have to mimic the html to js. Just write down html.

--- js/MyTitle.jsx
+++ js/MyTitle.jsx
@@ -6,10 +6,11 @@ var h1 = React.DOM.h1
 var MyTitle = React.createClass({
   // enhanced object literal syntax
   render () {
+    const style = {color: this.props.color}
     return (
-      div(null,
-        h1({style: {color: this.props.color}}, this.props.title)
-      )
+      <div>
+        <h1 style={style}>{this.props.title}</h1>
+      </div>
     )
   }
 })
