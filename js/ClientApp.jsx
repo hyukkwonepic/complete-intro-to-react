242982129316f1e2b6e5d235bf3e893bbe3ced05
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-20 10:38:32 +0900

    refactor to arrow function:
    this function is totally equivalent to previous function

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,7 +2,7 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 var MyTitle = require('./MyTitle')
 
-var MyFirstComponent = function () {
+var MyFirstComponent = () => {
   return (
     <div>
       <MyTitle title="whatevs" color="rebeccapurple" />
