0a02a62d6e7cbbfa18a85f802a3b1394eb07d2fd
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-20 13:00:49 +0900

    add standard lint to webpack preloader

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,11 +2,11 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 
 const App = () => (
-  <div className="app-container">
-    <div className="home-info">
-      <h1 className="title">svideo</h1>
-      <input className="search" type="text" placeholder="Search" />
-      <button className="browse-all"> or browse all!?!?!?</button>
+  <div className='app-container'>
+    <div className='home-info'>
+      <h1 className='title'>svideo</h1>
+      <input className='search' type='text' placeholder='Search' />
+      <button className='browse-all'> or browse all!?!?!?</button>
     </div>
   </div>
 )
