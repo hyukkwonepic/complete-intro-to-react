bb87b747d389e755b70afd02619919c4dc35358c
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-20 10:39:29 +0900

    refacotr without curly braces:
    This implicitly returns following statements.

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,14 +2,12 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 var MyTitle = require('./MyTitle')
 
-var MyFirstComponent = () => {
-  return (
-    <div>
-      <MyTitle title="whatevs" color="rebeccapurple" />
-      <MyTitle title="LOL" color="papayawhip" />
-      <MyTitle title="OMGSHEISSOANNOYING" color="red" />
-    </div>
-  )
-}
+var MyFirstComponent = () => (
+  <div>
+    <MyTitle title="whatevs" color="rebeccapurple" />
+    <MyTitle title="LOL" color="papayawhip" />
+    <MyTitle title="OMGSHEISSOANNOYING" color="red" />
+  </div>
+)
 
 ReactDOM.render(<MyFirstComponent />, document.getElementById('app'))
