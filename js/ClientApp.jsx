12865836bf5aa20f741f0b5e3827afae8d95408c
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-20 12:28:50 +0900

    init webpack watch

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -6,7 +6,7 @@ const App = () => (
     <div className="home-info">
       <h1 className="title">svideo</h1>
       <input className="search" type="text" placeholder="Search" />
-      <button className="browse-all"> or browse all</button>
+      <button className="browse-all"> or browse all!?!?!?</button>
     </div>
   </div>
 )
