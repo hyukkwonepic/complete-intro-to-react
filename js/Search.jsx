f4f64dd81ca73a24931e5b264fcd71dd19532d49
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-21 13:55:53 +0900

    update component to use createClass

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,14 +2,22 @@ const React = require('react')
 const ShowCard = require('./ShowCard')
 const data = require('../public/data')
 
-const Search = () => (
-  <div className='container'>
-    <div className='shows'>
-      {data.shows.map((show) => (
-        <ShowCard {...show} key={show.imdbID} />
-      ))}
-    </div>
-  </div>
-)
+const Search = React.createClass({
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>svideo</h1>
+          <input className='search-input' type='text' placeholder='Search' />
+        </header>
+        <div className='shows'>
+          {data.shows.map((show) => (
+            <ShowCard {...show} key={show.imdbID} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Search
