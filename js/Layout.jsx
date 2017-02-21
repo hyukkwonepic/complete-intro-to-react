2b26d1bb97f52fcfdb24b70ab20ffbb6223eeb63
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-21 13:40:36 +0900

    update router:
    the very top route of '/' renders Layout component. As it has
    props.childrens, its child routes would be rendered at the position.
    When user access the route '/', its child indexroute would be rendered
    as a default. But both indexroute(Landing) and route(Search) has the
    same layout.

--- js/Layout.jsx
+++ js/Layout.jsx
@@ -2,14 +2,13 @@ const React = require('react')
 
 const Layout = (props) => (
   <div className='app-container'>
-    <h1>My branding</h1>
     {props.children}
   </div>
 )
 
 const { element } = React.PropTypes
 
-Layout.React.propTypes = {
+Layout.propTypes = {
   children: element.isRequired
 }
 
