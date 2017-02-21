0a689e494a1f85203ab01aeb8b73028714f58cad
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-21 11:24:55 +0900

    update result list:
    each data is shown in each data instance's card. the img src here, we
    used es6 template string which is closed with back-trick(`) and this
    gets useful when using js variables or function as a string.

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,9 +3,18 @@ const data = require('../public/data')
 
 const Search = () => (
   <div className='container'>
-    {data.shows.map((show) => (
-      <h3>{show.title}</h3>
-    ))}
+    <div className='shows'>
+      {data.shows.map((show) => (
+        <div className='show-card'>
+          <img src={`public/img/posters/${show.poster}`} className='show-card-img' />
+          <div className='show-card-text'>
+            <h3 className='show-card-title'>{show.title}</h3>
+            <h4 className='show-card-year'>({show.year})</h4>
+            <p className='show-card-description'>{show.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
   </div>
 )
 
