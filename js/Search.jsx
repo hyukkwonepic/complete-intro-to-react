5fa93e64af3bc194b69a80c70bd1b489d6ffdf69
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-21 14:27:44 +0900

    init state in component:
    initialize state with getInitialState method. The input's value is set
    to searchTerm state. When user change the text of input, it calls
    onChange and it triggers handleSearchTermEvent method then it set
    searchTerm state newly typed text.

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,13 +2,23 @@ const React = require('react')
 const ShowCard = require('./ShowCard')
 const data = require('../public/data')
 
-class Search extends React.Component {
+const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'this is my search term'
+    }
+  },
+  handleSearchTermEvent (e) {
+    this.setState({
+      searchTerm: e.target.value
+    })
+  },
   render () {
     return (
       <div className='container'>
         <header className='header'>
-          <h1 className='brand'>svideo</h1>
-          <input className='search-input' type='text' placeholder='Search' />
+          <h1 className='brand'>{this.state.searchTerm}</h1>
+          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
         </header>
         <div className='shows'>
           {data.shows.map((show) => (
@@ -18,6 +28,6 @@ class Search extends React.Component {
       </div>
     )
   }
-}
+})
 
 module.exports = Search
