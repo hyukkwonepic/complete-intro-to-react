7e8866cabf349db263fdf5344f94cc2694bb46aa
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-22 13:26:16 +0900

    create new component: route with param
    New component details is routed with /details/:id
    
    The value passed in at the location of :id is passed as props.param to the
    component.

---
+++ js/Details.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+
+class Details extends React.Component {
+  render () {
+    return (
+      <div className='container'>
+      </div>
+    )
+  }
+}
+
+module.exports = Details
