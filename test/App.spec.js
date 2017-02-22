392041ea4b3b331f932af09417c15a7a0a6857b2
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-22 11:15:04 +0900

    init mocha enzyme test

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,9 +1,13 @@
 /* eslint-env mocha */
 
 const { expect } = require('chai')
+const React = require('react')
+const Search = require('../js/Search')
+const { shallow } = require('enzyme')
 
 describe('<Search />', () => {
-  it('should pass', () => {
-    expect(1 + 1).to.equal(5)
+  it('should render the brand', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.contains(<h1 className='brand'>svideo</h1>)).to.be.true
   })
 })
