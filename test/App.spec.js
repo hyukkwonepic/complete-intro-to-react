0200d7736e719820526d9d46d7b2a19c47c86b98
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-22 12:31:36 +0900

    add test: using mount
    for interactive testing, instead of shallow, mount should be used.

--- test/App.spec.js
+++ test/App.spec.js
@@ -5,7 +5,7 @@ const React = require('react')
 const Search = require('../js/Search')
 const ShowCard = require('../js/ShowCard')
 
-const { shallow } = require('enzyme')
+const { shallow, mount } = require('enzyme')
 const { shows } = require('../public/data')
 
 describe('<Search />', () => {
@@ -17,5 +17,14 @@ describe('<Search />', () => {
   it('should render as many shows as there are data for', () => {
     const wrapper = shallow(<Search />)
     expect(wrapper.find(ShowCard).length).to.equal(shows.length)
+  })
+
+  it('should filter correctly given new state', () => {
+    const wrapper = mount(<Search />)
+    const input = wrapper.find('.search-input')
+    input.node.value = 'house'
+    input.simulate('change')
+    expect(wrapper.state('searchTerm')).to.equal('house')
+    expect(wrapper.find('.show-card').length).to.equal(2)
   })
 })
