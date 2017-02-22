d3b11d3405d8f0500412cfb15c7696e02a344964
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-22 12:02:11 +0900

    add test:

--- test/App.spec.js
+++ test/App.spec.js
@@ -3,11 +3,19 @@
 const { expect } = require('chai')
 const React = require('react')
 const Search = require('../js/Search')
+const ShowCard = require('../js/ShowCard')
+
 const { shallow } = require('enzyme')
+const { shows } = require('../public/data')
 
 describe('<Search />', () => {
   it('should render the brand', () => {
     const wrapper = shallow(<Search />)
     expect(wrapper.contains(<h1 className='brand'>svideo</h1>)).to.be.true
+  })
+
+  it('should render as many shows as there are data for', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.find(ShowCard).length).to.equal(shows.length)
   })
 })
