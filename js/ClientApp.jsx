cd3136fe50ed67e2c2777f311aff6f32f4d106ae
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-23 11:13:25 +0900

    init onEnter prop for redirection:
    the passed in function to onEnter is called when a route is about to be
    entered. In this case, when user access to detail route, the assignShow
    function is activated. This function checks if requested url id matches
    any of instances of data set. If none of them matches, it redirects to
    main(/) page. If any data was found, the data is passed into params.

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -7,14 +7,28 @@ const Details = require('./Details')
 const { Router, Route, IndexRoute, hashHistory } = require('react-router')
 const { shows } = require('../public/data')
 
-const App = () => (
-  <Router history={hashHistory}>
-    <Route path='/' component={Layout}>
-      <IndexRoute component={Landing} />
-      <Route path='/search' component={Search} shows={shows} />
-      <Route path='/details/:id' component={Details} />
-    </Route>
-  </Router>
-)
+const App = React.createClass({
+  assignShow (nextState, replace) {
+    const showArray = shows.filter((show) => show.imdbId === nextState.id)
+
+    if (showArray.length < 1) {
+      return replace('/')
+    }
+
+    Object.assign(nextState.params, showArray[0])
+    return nextState
+  },
+  render () {
+    return (
+      <Router history={hashHistory}>
+        <Route path='/' component={Layout}>
+          <IndexRoute component={Landing} />
+          <Route path='/search' component={Search} shows={shows} />
+          <Route path='/details/:id' component={Details} onEnter={this.assignShow} />
+        </Route>
+      </Router>
+    )
+  }
+})
 
 ReactDOM.render(<App />, document.getElementById('app'))
